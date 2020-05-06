499145cf348952e2b81bcfbd7475e9687e9f8464
eslingsby <elliottslingsby@googlemail.com>
2020-05-06 01:54:20 +0100

    Basic select functionality (css broke, no deleting or creating entries)

--- main.js
+++ main.js
@@ -1,69 +1,68 @@
 const context = {
+    // Made these in the same scope as handlebars so you can render them with conditionals
+    // 0 by default, might need to do some checks if the context is ever empty later on
+    "activeCategory": 0,
+    "activeSubCategory": 0,
+
+    // Rest of context is the same except no manual ids
     "categories": [
         {
-            "id": 0,
             "title": "Code References",
-            "selected": true,
             "content": "something in the category notes page",
             "subcategories": [
                 {
-                    "id": 0,
                     "title": "powershell",
-                    "content": "some notes on github",
+                    "content": "some notes on powershell",
                 },
                 {
-                    "id": 1,
                     "title": "bash", 
-                    "content": "some notes on powershell", 
+                    "content": "some notes on bash", 
                 }
             ],
         },
         {
-            "id": 1,
             "title": "Misc Notes",
-            "selected": false,
             "content": "something in",
             "subcategories": [
                 {
-                    "id": 0,
                     "title": "notes",
                     "content": "some notes",
                 },
                 {
-                    "id": 1,
                     "title": "todo", 
-                    "content": "some notes on powershell", 
+                    "content": "some notes on eggs", 
                 },
                 {
-                    "id": 2,
                     "title": "funstuff", 
-                    "content": "some notes on powershell", 
+                    "content": "some notes on vr", 
                 }
 
             ],
         },
         {
-            "id": 2,
             "title": "Study References",
-            "selected": false,
             "content": "something in the category notes page",
             "subcategories": [
                 {
-                    "id": 0,
                     "title": "law",
-                    "content": "some notes on github",
+                    "content": "some notes on rat law",
                 },
                 {
-                    "id": 1,
                     "title": "cs", 
-                    "content": "some notes on powershell", 
+                    "content": "some notes on hoops", 
                 }
             ],
         },
     ],
 }
 
-// Handlebars logic
+// https://stackoverflow.com/questions/34252817/handlebarsjs-check-if-a-string-is-equal-to-a-value
+// Handlebars should really have this stuff by default imho
+Handlebars.registerHelper("ifEquals", function(arg1, arg2, options) {
+    return (arg1 == arg2) ? options.fn(this) : options.inverse(this);
+});
+
+// Handlebars setup
 const source = document.getElementById("templateHB").innerHTML;
 const template = Handlebars.compile(source);
 const compiledHtml = template(context);
@@ -71,112 +70,37 @@ const app = document.getElementById("app")
 app.innerHTML = (compiledHtml);
 
 // recompile Hanldlebars template
-const compileTemplate = () => {
+function compileTemplate() {
     const compiledHtml = template(context);
     app.innerHTML = (compiledHtml);
     console.log("Recompiled DOM")
 }
 
-// global
-var activeCategory = "";
-var activeSubcategory = "";
+function selectCategory(index) {
+    if (context.activeCategory == index)
+        return
 
-// set active to 1st category
-window.onload = (event) => {
-    if(document.getElementById("0")) {
-        activeCategory = document.getElementById("0")
-        activeCategory.classList.add("active");
-    } 
-    if(document.getElementById("s0")) {
-        activeSubcategory = document.getElementById("s0");
-        activeSubcategory.classList.add("active");
-    }
-};
-
-// Select Logic 
-const selectCategory = () => {
-    const entry = event.target;
-
-    if (entry.id != activeCategory.id) {
-        activeCategory.classList.remove("active");
-        activeCategory = entry;
-        activeCategory.classList.add("active")
-    }
-
-    console.log(entry.id + activeCategory.id);
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// CRUD logic
-const createEntry = () => {
-    // get user input
-    const entry = prompt("New entry:");
-
-    if(entry == null) {
-        alert("Can't be empty")
-    } else {
-        if(event.target.id == "newCategoryButton") {
-            context.categories.push({
-                "id": context.categories.length,
-                "title": entry,
-                "selected": false,
-                "subcategories": [],
-            })
-        } else if (event.target.id == "newSubcategoryButton") {
-            context.categories[activeCategory.id].subcategories.push({
-                "id": context.categories[activeCategory.length],
-                "title": entry,
-                "selected": false,
-            })
-        }
-    }
-    assignID();
+    context.activeCategory = index;
+    context.activeSubCategory = 0;
     compileTemplate();
-    console.log("Entry created")
-    console.log(event.target.id)
-    console.log(context);
 }
 
-const deleteEntry = () => {
-    // remove from DOM
-    activeCategory.parentNode.removeChild(activeCategory)
+function selectSubCategory(index) {
+    if (context.activeSubCategory == index)
+        return
 
-    // remove from context
-    context.categories.splice(entry.id, 1);
-    assignID();
-
-    console.log("Entry deleted")
+    context.activeSubCategory = index;
+    compileTemplate();
 }
 
-const updateEntry = () => {
-    // broken logic;
+function createEntry() {
 
 }
 
-// reassign IDs to be linearly consisten
-const assignID = () => {
-    for(i = 0; i < context.categories.length; i++) {
-        if(context.categories[i].id != i) {
-            context.categories[i].id = i;
-        }
-    }
-};
+function deleteEntry() {
 
+}
 
+function updateEntry() {
 
-
+}
